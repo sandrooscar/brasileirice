d292f09427a30abc1e592d1b1769ab0f5b5e2129
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2019-12-04 20:40:27 -0300

    AULA01-Validando CPF, CNPJ e Título de Eleitor com lib Stella

--- src/main/java/br/com/alura/ValidacaoDocumento.java
+++ src/main/java/br/com/alura/ValidacaoDocumento.java
@@ -1,19 +1,32 @@
 package br.com.alura;
 
+import br.com.caelum.stella.validation.CNPJValidator;
 import br.com.caelum.stella.validation.CPFValidator;
 import br.com.caelum.stella.validation.InvalidStateException;
+import br.com.caelum.stella.validation.TituloEleitoralValidator;
+import br.com.caelum.stella.validation.Validator;
 
 public class ValidacaoDocumento {
 
 	public static void main(String[] args) {
-//		String cpf = "86288366757";
-		String cpf = "86288366751";
-		CPFValidator validador = new CPFValidator();
+		String cpf = "86288366757";
+//		String cpf = "86288366751";
+		validarDocumentos(new CPFValidator(), cpf);
+
+		String cnpj = "47324831000182";
+		validarDocumentos(new CNPJValidator(), cnpj);
+
+		String tituloEleitor = "47324831000182";
+		validarDocumentos(new TituloEleitoralValidator(), tituloEleitor);
+		
+	}
+
+	private static void validarDocumentos(Validator<String> validador, String documento) {
 		try {
-			validador.assertValid(cpf);
-			System.out.println("CPF válido");
-		}catch (InvalidStateException e) {
-			System.out.println("CPF inválido: "+e.toString());
+			validador.assertValid(documento);
+			System.out.println("Documento válido");
+		} catch (InvalidStateException e) {
+			System.out.println("Documento inválido");
 		}
 	}
 
