b84f5db1e41499163da39f6285f9b88c9ea0ad6a
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2019-12-04 21:22:54 -0300

    AULA01-Formatação do CPF e CNPJ-lib Stella

---
+++ src/main/java/br/com/alura/FormatadorDocumento.java
@@ -0,0 +1,36 @@
+package br.com.alura;
+
+import br.com.caelum.stella.format.CNPJFormatter;
+import br.com.caelum.stella.format.CPFFormatter;
+import br.com.caelum.stella.format.Formatter;
+import br.com.caelum.stella.format.TituloEleitoralFormatter;
+
+public class FormatadorDocumento {
+
+	public static void main(String[] args) {
+		String cpf = "862.883.667-57";
+		String cnpj = "47.324.831/0001-82";
+		String tituloEleitor = "774349832321";
+		System.out.println(desformatarDocumentos(new CPFFormatter(), cpf));
+		System.out.println(desformatarDocumentos(new CNPJFormatter(), cnpj));
+		System.out.println(formatarDocumentos(new TituloEleitoralFormatter(), tituloEleitor));
+	}
+	
+	private static String formatarDocumentos(Formatter formatter, String documento) {
+		try {
+			return formatter.format(documento);
+		}catch (IllegalArgumentException e) {
+			System.out.println("Formato inválido de documento");
+		}
+		return "";
+	}
+	private static String desformatarDocumentos(Formatter formatter, String documento) {
+		try {
+			return formatter.unformat(documento);
+		}catch (IllegalArgumentException e) {
+			System.out.println("Formato inválido de documento");
+		}
+		return "";
+	}
+
+}
