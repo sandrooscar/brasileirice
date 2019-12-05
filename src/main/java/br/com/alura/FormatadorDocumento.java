69852fbc0f18bafb318486a8e02a90207d364748
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2019-12-04 21:37:57 -0300

    AULA01-Métodos para formatação-Caelum-stella

--- src/main/java/br/com/alura/FormatadorDocumento.java
+++ src/main/java/br/com/alura/FormatadorDocumento.java
@@ -3,6 +3,7 @@ package br.com.alura;
 import br.com.caelum.stella.format.CNPJFormatter;
 import br.com.caelum.stella.format.CPFFormatter;
 import br.com.caelum.stella.format.Formatter;
+import br.com.caelum.stella.format.NITFormatter;
 import br.com.caelum.stella.format.TituloEleitoralFormatter;
 
 public class FormatadorDocumento {
@@ -11,9 +12,11 @@ public class FormatadorDocumento {
 		String cpf = "862.883.667-57";
 		String cnpj = "47.324.831/0001-82";
 		String tituloEleitor = "774349832321";
+		String nit = "77439741678";//pis/pasep
 		System.out.println(desformatarDocumentos(new CPFFormatter(), cpf));
 		System.out.println(desformatarDocumentos(new CNPJFormatter(), cnpj));
 		System.out.println(formatarDocumentos(new TituloEleitoralFormatter(), tituloEleitor));
+		System.out.println(formatarDocumentos(new NITFormatter(), nit));
 	}
 	
 	private static String formatarDocumentos(Formatter formatter, String documento) {
